cb498033b8c0e7fd8e0d76cd80d281528d07f980
ZaidanNur <zaixyz332@gmail.com>
2022-12-01 12:25:53 +0700

    init cms template

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CmsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('cms')->name('cms.')->group(function () {
+    Route::get('/index',[CmsController::class,'index'])->name('index');
+    // Route::resource('user', UserController::class);
 });
